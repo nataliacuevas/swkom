eaa77b7cadefc1d8b88351d5d74e1f9744145f22
donyaMoharam <moharamdonya@gmail.com>
2024-09-30 18:06:53 +0200

    nginx integration and webpage comminucation with REST server

---
+++ sws/wwwroot/script.js
@@ -0,0 +1,10 @@
+﻿document.getElementById('fetch-message').addEventListener('click', function () {
+    fetch('/api/helloworld')  // Call the REST endpoint
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('api-message').textContent = data;
+        })
+        .catch(error => {
+            document.getElementById('api-message').textContent = 'Error: Unable to fetch message from API';
+        });
+});
